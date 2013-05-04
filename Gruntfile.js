ad63ff3a925c28b5b0beecf2803c5a9efa260292
Marco Trulla <marco@marcotrulla.it>
2013-05-04 19:17:14 +0200

    Update settings files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,73 +2,73 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    clean: {
-      markdown: ['./md']
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      bin: {
-        src: ['bin/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      },
-    },
-    markdown: {
-      options: {
-        dest: './md/'
-      },
-      files: [ './*.md', './doc/*.md' ]
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      bin: {
-        files: '<%= jshint.bin.src %>',
-        tasks: ['jshint:bin', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
-      markdown: {
-        files: '<%= markdown.files %>',
-        tasks: ['clean:markdown', 'markdown']
-      }
-    },
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+			markdown: ['./md']
+		},
+		nodeunit: {
+			files: ['test/**/*_test.js'],
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib: {
+				src: ['lib/**/*.js']
+			},
+			bin: {
+				src: ['bin/**/*.js']
+			},
+			test: {
+				src: ['test/**/*.js']
+			},
+		},
+		markdown: {
+			options: {
+				dest: './md/'
+			},
+			files: [ './*.md', './doc/*.md' ]
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			lib: {
+				files: '<%= jshint.lib.src %>',
+				tasks: ['jshint:lib', 'nodeunit']
+			},
+			bin: {
+				files: '<%= jshint.bin.src %>',
+				tasks: ['jshint:bin', 'nodeunit']
+			},
+			test: {
+				files: '<%= jshint.test.src %>',
+				tasks: ['jshint:test', 'nodeunit']
+			},
+			markdown: {
+				files: '<%= markdown.files %>',
+				tasks: ['clean:markdown', 'markdown']
+			}
+		},
+	});
 
-  // Grunt-Contrib Tasks
-  Object.keys(grunt.config('pkg').devDependencies).forEach(function(dep){
-    if (/^grunt\-/i.test(dep)) {
-      grunt.loadNpmTasks( dep );
-    } // if
-  });
+	// Grunt-Contrib Tasks
+	Object.keys(grunt.config('pkg').devDependencies).forEach(function(dep){
+		if (/^grunt\-/i.test(dep)) {
+			grunt.loadNpmTasks( dep );
+		} // if
+	});
 
-  // Custom tasks
-  grunt.loadTasks( 'tasks' );
+	// Custom tasks
+	grunt.loadTasks( 'tasks' );
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'nodeunit']);
 
 };
